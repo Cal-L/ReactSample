b26fbfabe820d46e86db6e05945a87b960cd4f35
Cal-L <cleun007@gmail.com>
2017-01-14 01:33:30 -0800

    Add new card file

---
+++ src/Card.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={styles.card}></div>
+    );
+  }
+}
+
+const styles = {
+  card: {
+    margin: 10,
+    height: 75,
+    width: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  }
+}
+
+export default Card;
